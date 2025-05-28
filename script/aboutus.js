46b430780ab7fa457a13791ec6675e9245a58679
MohamedChaibi <hamma_chaibi@yahoo.fr>
2025-05-28 09:24:08 +0100

    missing js buy and sell car

--- script/aboutus.js
+++
@@ -1,5 +0,0 @@
-// --------------- About Us -------------- 
-function toggleBio(element) {
-    var bio = element.querySelector('.bio');
-    bio.style.display = bio.style.display === 'block' ? 'none' : 'block';
-}
--- script/buy.js
+++ script/buy.js
@@ -17,7 +17,6 @@ localStorage.setItem("credentials",JSON.stringify([{email:email,password:passwor
 
 var credentials=JSON.parse(localStorage.getItem("credentials"))
 //  credentials
-
 console.log( credentials.email)
 
 // profile
@@ -29,3 +28,28 @@ function editProfile() {
     if (newEmail) document.getElementById("email").textContent = newEmail;
 }
 
+// --------------- About Us -------------- 
+// selecting class and reasign style
+function toggleBio(element) {
+    var bio = element.querySelector('.bio');
+    bio.style.display = bio.style.display === 'block' ? 'none' : 'block';
+}
+
+// buy car
+
+// sell
+var sellCar = function sellCar() {
+    var instance={}
+    instance.name=
+    instance.email=
+    instance.carName=
+    instance.description=
+    instance.price=0
+    return instance
+}
+
+var tax = 0.05
+
+function TotalWithTaxe(tax){
+    return price + ( price * tax )
+}
